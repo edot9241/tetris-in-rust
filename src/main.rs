6c1804dc426345148ec63440226bb2cb9bff3e37
polly <pollyvx@gmail.com>
2025-01-20 23:25:50 +0100

    Add Window Functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use bevy::{
+    core::FrameCount,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowLevel, WindowTheme},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Tetris".into(),
+                    name: Some("bevy.app".into()),
+                    resolution: (500., 300.).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    resizable: false,
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    window_theme: Some(WindowTheme::Dark),
+                    enabled_buttons: bevy::window::EnabledButtons {
+                        maximize: false,
+                        minimize: false,
+                        ..Default::default()
+                    },
+                    visible: false,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
+        .add_systems(Update, (switch_level, make_visible))
+        .run();
+}
+
+fn make_visible(mut window: Single<&mut Window>, frames: Res<FrameCount>) {
+    if frames.0 == 3 {
+        window.visible = true;
+    }
+}
+
+fn switch_level(input: Res<ButtonInput<KeyCode>>, mut window: Single<&mut Window>) {
+    if input.just_pressed(KeyCode::KeyT) {
+        window.window_level = match window.window_level {
+            WindowLevel::AlwaysOnBottom => WindowLevel::Normal,
+            WindowLevel::Normal => WindowLevel::AlwaysOnTop,
+            WindowLevel::AlwaysOnTop => WindowLevel::AlwaysOnBottom,
+        };
+        info!("WINDOW_LEVEL: {:?}", window.window_level);
+    }
 }
